dcbe51f3f26e5604efd2edb093f8501d9bb1b089
Pranjal Patra <pranjal.patra.2022@gmail,com>
2022-01-10 10:42:47 -0330

    add exception handling and assertions

--- review/functions.py
+++ review/functions.py
@@ -1,10 +1,15 @@
 def restaurant_bouncer(age:int, fully_vaxed:bool = False) -> bool:
     """
     checks if the person is allowed to enter restaurant or not.
+    raises exception if age <= 0 and not int
+    raises exception if fully_vaxed is not a bool
     :param age: (int) age of the individual
-    :param fully_vaxed: (bool) vaccination status
+    :param fully_vaxed: (bool) vaccination status, default is False
     :return: (bool) True if allowed else False
     """
+    assert isinstance(age, int), 'age must be an int'
+    assert isinstance(fully_vaxed, bool), 'fully_vaxed must be a bool'
+    assert age >= 0, 'age cannot be negative'
     if fully_vaxed or age < 18:
         print('Allowed to enter restaurant')
         return True
@@ -12,10 +17,14 @@ def restaurant_bouncer(age:int, fully_vaxed:bool = False) -> bool:
         print('Sorry, not allowed in')
         return False
 
-# print(help(restaurant_bouncer))
-# restaurant_bouncer(20, True)
-# restaurant_bouncer(4, False)
-# restaurant_bouncer(60, True)
 
-
-restaurant_bouncer('60', 3.14)
+while True:
+    try:
+        age = int(input('Enter Age'))
+        vaccine_count = int(input('Enter vaccines taken: '))
+        if vaccine_count >= 2:
+            double_vaccinated = True
+        restaurant_bouncer(age, double_vaccinated)
+        break
+    except Exception as e:
+        print(e)
