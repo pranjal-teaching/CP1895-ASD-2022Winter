217a9fd06997da4b992f09fefbf5d7686df31bea
Eric Thomas <ericthomas@tuta.io>
2022-01-15 20:25:14 -0330

    finished up question1.py

--- student_code/Eric_Thomas/question1.py
+++ student_code/Eric_Thomas/question1.py
@@ -1,5 +1,20 @@
-user = {
-    'first_name': input("Please enter your first name: "),
-    'last_name': input("Please enter your last name: "),
-    'age': input("Please enter your age: ")
-}
+def generate_table(a, b):
+    columns = 1
+    while columns <= a:
+        rows = 1
+        while rows <= b:
+            print("%4d" % (columns * rows), end=' ')
+            rows += 1
+        print()
+        columns += 1
+
+
+def main():
+    print("Multiplication Table Generator \n")
+    a = int(input("Enter number of rows as an integer: "))
+    b = int(input("Enter number of columns as an integer: "))
+    generate_table(a, b)
+
+
+if __name__ == "__main__":
+    main()
