e1b550753325a07b2db16f65003f39b79123b92d
Pranjal Patra <pranjal.cna@gmail.com>
2022-01-31 09:01:48 -0330

    product class

---
+++ 3_OOP/3_products.py
@@ -0,0 +1,18 @@
+class Product:
+    def __init__(self, name, price, discountPercent):
+        self.name = name
+        self.price = price
+        self.discountPercent = discountPercent
+
+    def getDiscountAmount(self):
+        return self.price - self.getDiscountPrice()
+
+    def getDiscountPrice(self):
+        return (self.price * self.discountPercent) / 100
+
+
+product1 = Product(name="Stanley Hammer", price=12.99, discountPercent=62)
+prodcut2 = Product(name='nails', price=5.06, discountPercent=0)
+
+print(product1.getDiscountPrice())
+print(product1.getDiscountAmount())
