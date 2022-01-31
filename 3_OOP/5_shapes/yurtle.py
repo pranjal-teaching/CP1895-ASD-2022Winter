7296ef8a125c06b0f088babfa50fc948b9335705
Pranjal Patra <pranjal.cna@gmail.com>
2022-01-31 11:13:52 -0330

    turtle demo

---
+++ 3_OOP/5_shapes/yurtle.py
@@ -0,0 +1,15 @@
+import time
+from turtle import *
+
+yurtle = Turtle()
+
+time.sleep(3)
+yurtle.circle(20)
+yurtle.color('red')
+yurtle.pensize(10)
+yurtle.penup()
+yurtle.goto(100, 100)
+yurtle.pendown()
+yurtle.circle(50)
+
+done()
