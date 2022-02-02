5e4e95a5fd2d7c2963c9778258b15c3f1ed1eb03
Pranjal Patra <pranjal.cna@gmail.com>
2022-02-02 14:04:42 -0330

    add dataclass city

---
+++ 3_OOP/6_composition/city.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class City:
+    population: int
+    name: str
+    tax_rate: float
+
+    def collect_taxes(self):
+        return self.tax_rate * self.population * 500
+
+
+if __name__ == '__main__':
+    st_johns = City(population=100000, name="St. John's", tax_rate=30)
